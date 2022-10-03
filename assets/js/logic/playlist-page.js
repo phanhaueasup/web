33ed627a6d0cc924dbdbd8564247329586309300
s <s>
2022-10-03 15:28:22 +0700

    add tags

--- assets/js/logic/playlist-page.js
+++ assets/js/logic/playlist-page.js
@@ -117,7 +117,7 @@ let playlistPage=function(pathname){
                 <div class="card-body">
                    <h5 class="card-title">${data[i]["title"]}</h5>
 
-                   <i id="tags" class="bi bi-tags-fill"></i>  <a href="" rel="tag">Big tits,</a> <a href="" rel="tag">Big tits</a><br/>
+                   <i class="bi bi-tags-fill"></i>  <a href="" rel="tag">Big tits,</a> <a href="" rel="tag">Big tits</a><br/>
                    <i class="bi bi-eye-fill"></i> ${view_number} views <br/>
                    <i class="bi bi-calendar-event"></i> ${data[i]["date_added"].split("T")[0]}
                 </div>
@@ -129,7 +129,7 @@ let playlistPage=function(pathname){
       // add tags
       for(let j=0;j<data[i]["tags"].length;j++){
         if(data[i]["tags"][j].length<=12){
-          $("#tags").append(`<a href="">${data[i]["tags"][j]},</a>`);
+          $(".bi-tags-fil").eq(i).append(`<a href="">${data[i]["tags"][j]},</a>`);
           if(j<data[i]["tags"].length-1) $("#tags").append(`,</a>`)
           else $("#tags").append(`</a>`)
         }
