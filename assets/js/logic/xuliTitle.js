f13fcded7b6abb743e934054b89ddc3c2dc893e5
s <s>
2022-10-03 08:54:56 +0700

    change ./assets to assets

--- assets/js/logic/xuliTitle.js
+++ assets/js/logic/xuliTitle.js
@@ -2,5 +2,19 @@ xuliTitle=function(pathname){
   if(pathname=="/"){
     document.title="stealimage.com - share beautiful, high-quality photos. The largest number of photos in the world."
     $('head').append(`<meta name="Selection of the best photos, many genres and carefully updated online selection. censored, uncensored, beauty.">`);
+  }else if(pathname=="/studio"||
+          pathname=="/model"||
+          pathname=="/category"||
+          pathname=="/studio/"||
+          pathname=="/model/"||
+          pathname=="/category/"){
+          categoryPage(pathname)
+  }else if(pathname.includes("/studio")||
+          pathname.includes("/model")||
+          pathname.includes("/category")){
+          categoryPlaylistPage(pathname)
+
+  }else{
+    playlistImagesPage(pathname)
   }
 }
