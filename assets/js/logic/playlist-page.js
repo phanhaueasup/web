22b3b1055459eb7d9dfadefbb7e05b99fccdd944
s <s>
2022-10-04 04:34:46 +0700

    sss

--- assets/js/logic/playlist-page.js
+++ assets/js/logic/playlist-page.js
@@ -75,7 +75,16 @@ let playlistPage=function(pathname){
            <!--   ads    -->
            <div class="card">
              <div class="card-body ads-card">
-              
+               <script type="text/javascript">
+                   atOptions = {
+                     'key' : '49f5d0a0cba27e3349bde680da73453e',
+                     'format' : 'iframe',
+                     'height' : 250,
+                     'width' : 300,
+                     'params' : {}
+                   };
+                   document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/49f5d0a0cba27e3349bde680da73453e/invoke.js"></scr' + 'ipt>');
+                 </script>
              </div>
            </div>
 
